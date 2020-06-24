7a982b84d949082d1dfa6782f0a5874340553880
Randy Zwitch <rzwitch@gmail.com>
2020-06-24 14:17:45 -0400

    Cleanup license and setup

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 import setuptools
 
 setuptools.setup(
-    name="streamlit-custom-component",
+    name="streamlit-victorycharts",
     version="0.0.1",
-    author="",
-    author_email="",
-    description="",
-    long_description="",
+    author="Randy Zwitch",
+    author_email="randy@streamlit.io",
+    description="Streamlit component for the Victory React.js library",
+    long_description="Streamlit component for the Victory React.js library (https://formidable.com/open-source/victory/)",
     long_description_content_type="text/plain",
-    url="",
+    url="https://github.com/randyzwitch/streamlit-victorycharts",
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=[],
